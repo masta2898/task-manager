ac1d142454b1e851f71429b1bd6116a4a74254b7
Master <kulikov.paul99@gmail.com>
2019-01-01 23:09:51 +0200

    init

---
+++ src/main/java/ua/edu/sumdu/j2se/levchenko/tasks/TaskList.java
@@ -0,0 +1,14 @@
+package ua.edu.sumdu.j2se.levchenko.tasks;
+
+import java.util.Date;
+
+abstract class TaskList implements Iterable<Task>{
+    protected int count = 0;
+    public abstract void add(Task task);
+    public abstract boolean remove(Task task);
+    public abstract Task getTask(int index);
+
+    public int size() {
+        return count;
+    }
+}
