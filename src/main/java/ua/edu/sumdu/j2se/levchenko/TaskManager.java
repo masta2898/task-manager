a5ad9e598813cf71b55c38dd14d1b27169c3bd88
Master <kulikov.paul99@gmail.com>
2019-01-02 19:24:51 +0200

    began working on gui

---
+++ src/main/java/ua/edu/sumdu/j2se/levchenko/TaskManager.java
@@ -0,0 +1,26 @@
+package ua.edu.sumdu.j2se.levchenko;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import ua.edu.sumdu.j2se.levchenko.controller.MainController;
+
+public class TaskManager extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setController(new MainController());
+        loader.setLocation(getClass().getResource("/main.fxml"));
+        Parent content = loader.load();
+
+        stage.setTitle("Task Manager");
+        stage.setScene(new Scene(content, 500, 400));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
