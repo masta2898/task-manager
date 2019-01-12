8814171597e226f7818e0495d34269a4a16c6634
Master <master2898thetimelord@gmail.com>
2019-01-12 23:05:20 +0200

    added show details

--- src/main/java/ua/edu/sumdu/j2se/levchenko/controller/Controller.java
+++ src/main/java/ua/edu/sumdu/j2se/levchenko/controller/Controller.java
@@ -1,9 +1,8 @@
 package ua.edu.sumdu.j2se.levchenko.controller;
 
-import javafx.collections.ObservableList;
-import ua.edu.sumdu.j2se.levchenko.TaskView;
+import javafx.stage.Stage;
 
 public interface Controller {
-    void setTaskTableObservableList(ObservableList<TaskView> taskTableObservableList);
-    void show();
+    void setWindow(Stage window);
+    void showWindow();
 }
--- src/main/java/ua/edu/sumdu/j2se/levchenko/controller/TaskDetailsController.java
+++ src/main/java/ua/edu/sumdu/j2se/levchenko/controller/TaskDetailsController.java
@@ -1,4 +1,76 @@
 package ua.edu.sumdu.j2se.levchenko.controller;
 
-public class TaskDetailsController {
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import ua.edu.sumdu.j2se.levchenko.tasks.Task;
+
+public class TaskDetailsController implements TaskOperationController {
+    @FXML
+    private Label title;
+
+    @FXML
+    private HBox timeBox;
+    @FXML
+    private Label time;
+
+    @FXML
+    private HBox repeatParametersBox;
+    @FXML
+    private Label start;
+    @FXML
+    private Label end;
+    @FXML
+    private Label interval;
+
+    @FXML
+    private CheckBox active;
+    @FXML
+    private CheckBox repeated;
+
+    private Task task;
+    private Stage detailsWindow;
+
+    @Override
+    public void setTask(Task task) {
+        this.task = task;
+    }
+
+    @Override
+    public Task getTask() {
+        return task;
+    }
+
+    @Override
+    public void setWindow(Stage window) {
+        this.detailsWindow = window;
+    }
+
+    @Override
+    public void showWindow() {
+        if (task == null) {
+            return;
+        }
+
+        title.setText(task.getTitle());
+
+        boolean taskIsRepeated = task.isRepeated();
+        repeated.setSelected(taskIsRepeated);
+        repeatParametersBox.setVisible(taskIsRepeated);
+        timeBox.setVisible(!taskIsRepeated);
+        active.setSelected(task.isActive());
+
+        if (taskIsRepeated) {
+            time.setText(task.getTime().toString());
+        } else {
+            start.setText(task.getStartTime().toString());
+            end.setText(task.getEndTime().toString());
+            interval.setText(String.valueOf(task.getRepeatInterval()));
+        }
+
+        detailsWindow.setResizable(false);
+        detailsWindow.showAndWait();
+    }
 }
