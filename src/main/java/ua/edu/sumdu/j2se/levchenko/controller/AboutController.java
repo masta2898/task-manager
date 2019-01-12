92b74c5b1eb87a813b205f0a37b7e3f72e7080de
Master <master2898thetimelord@gmail.com>
2019-01-12 23:06:05 +0200

    added about window

--- src/main/java/ua/edu/sumdu/j2se/levchenko/controller/AboutController.java
+++ src/main/java/ua/edu/sumdu/j2se/levchenko/controller/AboutController.java
@@ -1,25 +1,18 @@
 package ua.edu.sumdu.j2se.levchenko.controller;
 
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.stage.Stage;
-import ua.edu.sumdu.j2se.levchenko.TaskView;
 
 public class AboutController implements Controller {
     private Stage aboutWindow;
-    private ObservableList<TaskView> taskTableObservableList;
 
-    public AboutController(Stage aboutWindow) {
-        this.aboutWindow = aboutWindow;
+    @Override
+    public void setWindow(Stage window) {
+        this.aboutWindow = window;
     }
 
     @Override
-    public void setTaskTableObservableList(ObservableList<TaskView> taskTableObservableList) {
-        this.taskTableObservableList = taskTableObservableList;
-    }
-
-    @Override
-    public void show() {
+    public void showWindow() {
         aboutWindow.showAndWait();
     }
 
