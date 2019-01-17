0bfa4730b1dfa6684ad0e585f6a4c99e05972303
Master <master2898thetimelord@gmail.com>
2019-01-17 22:46:21 +0200

    started working on notificator

---
+++ src/main/java/ua/edu/sumdu/j2se/levchenko/notificator/Notificator.java
@@ -0,0 +1,4 @@
+package ua.edu.sumdu.j2se.levchenko.notificator;
+
+public interface Notificator {
+}
---
+++ src/main/java/ua/edu/sumdu/j2se/levchenko/notificator/TaskNotificator.java
@@ -0,0 +1,41 @@
+package ua.edu.sumdu.j2se.levchenko.notificator;
+
+import ua.edu.sumdu.j2se.levchenko.controller.NotificationObserverController;
+import ua.edu.sumdu.j2se.levchenko.tasks.TaskList;
+import ua.edu.sumdu.j2se.levchenko.tasks.repository.Repository;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Notificator extends Thread {
+    private final Repository repository;
+    private final NotificationObserverController notificationObserver;
+
+    public Notificator(final Repository repository, NotificationObserverController notificationObserver) {
+        this.repository = repository;
+        this.notificationObserver = notificationObserver;
+        setDaemon(true);
+    }
+
+    @Override
+    public void run() {
+        long delay = 1;
+        long oneMinuteInMillis = 60 * 1000;
+
+        while (true) {
+            Calendar now = Calendar.getInstance();
+            long nowInMillis = now.getTimeInMillis();
+            Date afterTenMinutes = new Date(nowInMillis + (delay * oneMinuteInMillis));
+
+            TaskList tasks = repository.getTasksByPeriod(now.getTime(), afterTenMinutes);
+            notificationObserver.showNotification(tasks);
+
+            try {
+                TimeUnit.MINUTES.sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
